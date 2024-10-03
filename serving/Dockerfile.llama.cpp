bc98a0c5fe6c1136906f769250b035d587d6f9bf
Mike Hepburn <eformat@gmail.com>
2024-10-03 15:09:09 +1000

    🐾 add another cluster demo 🐾

--- serving/Dockerfile.llama.cpp
+++ serving/Dockerfile.llama.cpp
@@ -1,7 +1,6 @@
 # 2024.1
 FROM quay.io/modh/cuda-notebooks@sha256:d8295bcf45a6a522f78c07dc62634a7775fc434621973bd81db243a5a63a1ffa AS build
 USER root
-RUN dnf -y install https://rpmfind.net/linux/epel/9/Everything/x86_64/Packages/n/nvtop-3.1.0-2.el9.x86_64.rpm
 WORKDIR /opt/app-root/src
 RUN git clone https://github.com/ggerganov/llama.cpp.git
 ENV LD_LIBRARY_PATH=/usr/lib64/:/usr/local/cuda-12.1/compat
@@ -13,6 +12,8 @@ FROM quay.io/modh/cuda-notebooks@sha256:d8295bcf45a6a522f78c07dc62634a7775fc4346
 COPY --from=build /opt/app-root/src/llama.cpp/build/ggml/src/libggml.so /libggml.so
 COPY --from=build /opt/app-root/src/llama.cpp/build/src/libllama.so /libllama.so
 COPY --from=build /opt/app-root/src/llama.cpp/build/bin/llama-server /llama-server
+USER root
+RUN dnf -y install https://rpmfind.net/linux/epel/9/Everything/x86_64/Packages/n/nvtop-3.1.0-2.el9.x86_64.rpm
 ENV LLAMA_ARG_HOST=0.0.0.0
 ENV LD_LIBRARY_PATH=/
 USER 1001
